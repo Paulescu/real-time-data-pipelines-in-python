01fba637271c1195301333d4da60493833cdf99d
Paulescu <plabartabajo@gmail.com>
2024-01-16 13:17:01 +0100

    Add logging from the start for trade_producer

--- trade_producer/main.py
+++ trade_producer/main.py
@@ -1,3 +1,10 @@
-from src.main import run
+import logging
 
+from src.main import run
+from src.utils import initialize_logger
+
+initialize_logger()
+
+logger = logging.getLogger()
+logger.info('Starting trade producer...')
 run()
